2893b35043c192163d5a635d38c7db18d210d58f
Víctor Plata Bello <victor.plata@estudiante.uam.es>
2021-06-17 14:46:15 +0200

    first commit, followup working

---
+++ bot.py
@@ -0,0 +1,107 @@
+import sys
+import json
+import argparse
+from google.cloud import dialogflow as dialogflow_v2
+
+trainingSaludos = ["Hola", "Saludos", "Que tal", "hey"]
+textoAvisame = ["hola, cuando estes listo, avisame y comenzara el test de trivia"]
+
+
+def create_intent(project_id, display_name, training_phrases_parts,
+                  message_texts, hijo=False, father=None):
+    """Create an intent of the given intent type."""
+
+    intents_client = dialogflow_v2.IntentsClient()
+
+    parent = dialogflow_v2.AgentsClient.agent_path(project_id)
+    print("padre: " + parent)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow_v2.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow_v2.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+    responses = []
+    # for message in message_texts:
+    message_response_txt = dialogflow_v2.Intent.Message.Text(text=message_texts)
+    # print (message_response_txt)
+    message_response = dialogflow_v2.Intent.Message(text=message_response_txt)
+    # print (message_response)
+    responses.append(message_response)
+    # print (responses)
+    text = dialogflow_v2.Intent.Message.Text(text=message_texts)
+    message = dialogflow_v2.Intent.Message(text=text)
+    print("id", father)
+    if hijo:
+        followup = dialogflow_v2.Intent.FollowupIntentInfo(followup_intent_name="custom")
+        intent = dialogflow_v2.Intent(
+            display_name=display_name,
+            training_phrases=training_phrases,
+            messages=responses,
+            output_contexts=[
+                dialogflow_v2.Context(
+                    name=dialogflow_v2.ContextsClient.context_path(display_name, "-", "next-output"), lifespan_count=1)],
+            #input_context_names=["projects/" + project_id + "/agent/sessions/-/contexts/name"],
+            webhook_state=dialogflow_v2.Intent.WebhookState.WEBHOOK_STATE_ENABLED,
+            # is_fallback=True,
+            parent_followup_intent_name=father,
+            followup_intent_info=[followup]
+
+        )
+    else:
+        intent = dialogflow_v2.Intent(
+            display_name=display_name,
+            training_phrases=training_phrases,
+            messages=responses,
+            output_contexts=[
+                dialogflow_v2.Context(
+                    name=dialogflow_v2.ContextsClient.context_path(display_name, "-", "next-output"), lifespan_count=1)],
+            #input_context_names=["projects/" + project_id + "/agent/sessions/-/contexts/name"],
+            webhook_state=dialogflow_v2.Intent.WebhookState.WEBHOOK_STATE_ENABLED,
+            # is_fallback=True,
+            #parent_followup_intent_name=parent + "/intents/" + id[0],
+            #followup_intent_info=[followup]
+
+        )
+
+    print(intent)
+    response = intents_client.create_intent(request={'parent': parent, 'intent': intent})
+
+    print('Intent created: {}'.format(response))
+    return response
+
+
+def _get_intent_ids(project_id, display_name):
+    # from google.cloud import dialogflow
+    intents_client = dialogflow_v2.IntentsClient()
+
+    parent = dialogflow_v2.AgentsClient.agent_path(project_id)
+    intents = intents_client.list_intents(request={'parent': parent})
+    intent_names = [
+        intent.name for intent in intents
+        if intent.display_name == display_name]
+
+    intent_ids = [
+        intent_name.split('/')[-1] for intent_name
+        in intent_names]
+
+    return intent_ids
+
+
+def main():
+    # print("Hello World!")
+    # print(sys.argv[1],sys.argv[2], sys.argv[3],sys.argv[4])
+    intent1 = create_intent(sys.argv[1], sys.argv[2], trainingSaludos, textoAvisame)
+    print("intent nombre ::: ", intent1.name)
+    preguntas = open('preguntas.json', encoding="utf-8").read()
+    preguntasJson = json.loads(preguntas)
+    intentAnt = intent1
+    for pregunta in preguntasJson['preguntas']:
+        intent = create_intent(sys.argv[1], pregunta["titulo"][0], pregunta["titulo"], pregunta["respuesta"]
+                               , True, intentAnt.name)
+        intentAnt = intent
+        print(pregunta)
+
+if __name__ == "__main__":
+    main()
